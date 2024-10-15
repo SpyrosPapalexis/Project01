bb50d20f890e3727b791dcc7e612cb8b6efa75b6
SpyrosPapalexis <sdi1900148@di.uoa.gr>
2024-10-15 20:40:40 +0300

    cmake

---
+++ CMakeFiles/cgalexec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cgalexec.dir/cgalexec.cpp.o"
+  "cgalexec"
+  "cgalexec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cgalexec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
