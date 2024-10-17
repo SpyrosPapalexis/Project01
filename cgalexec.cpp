f36eabff1a634fea55e1a8cdf2bddf2aa37226a2
SpyrosPapalexis <sdi1900148@di.uoa.gr>
2024-10-17 03:23:29 +0300

    constraints

---
+++ cgalexec.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+
+
+    string instance_uid;
+    int numpoints;
+    int* points_x;
+    int* points_x;
+    void* region_boundary;
+    int num_constraints;
+    void* additional_constraints;
+
+
+
+
+    return 0;
+}
