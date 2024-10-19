26fa1a47e7b7591241cc2229555befc9145b9b97
SpyrosPapalexis <sdi1900148@di.uoa.gr>
2024-10-19 18:23:45 +0300

    reading json using boost

--- cgalexec.cpp
+++ cgalexec.cpp
@@ -2,10 +2,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 #define BUFFER 1024
 
 using namespace std;
+using namespace boost::property_tree;
 
 int main(void){
 
@@ -15,21 +18,43 @@ int main(void){
     cout << "Enter file name:" << endl;
     cin >> filename;
 
-    int file;
-    if ((file = open(filename, O_RDONLY)) == -1) perror("can't open file");
+    ptree jsonData;
+    try {
+        read_json(filename, jsonData);
+    } catch (const json_parser_error &e) {
+        cerr << "error reading file: " << e.what() << endl;
+        return 1;
+    }
 
-    if (read(file,txtbuffer,BUFFER) == -1) perror("can't read");
+    string instance_uid = jsonData.get<string>("instance_uid");
 
-    string instance_uid;
-    int numpoints;
-    int* points_x;
-    int* points_y;
-    void* region_boundary;
-    int num_constraints;
-    void* additional_constraints;
+    int num_points = jsonData.get<int>("num_points");
 
+    vector<int> points_x;
+    for (auto& item : jsonData.get_child("points_x")) {
+        points_x.push_back(item.second.get_value<int>());
+    }
 
+    vector<int> points_y;
+    for (auto& item : jsonData.get_child("points_y")) {
+        points_y.push_back(item.second.get_value<int>());
+    }
 
+    vector<int> region_boundary;
+    for (auto& item : jsonData.get_child("region_boundary")) {
+        region_boundary.push_back(item.second.get_value<int>());
+    }
+
+    int num_constraints = jsonData.get<int>("num_constraints");
+
+    vector<vector<int>> additional_constraints;
+    for (auto& item : jsonData.get_child("additional_constraints")) {
+        vector<int> constraint;
+        for (auto& sub_item : item.second) {
+            constraint.push_back(sub_item.second.get_value<int>());
+        }
+        additional_constraints.push_back(constraint);
+    }
 
 
     return 0;
