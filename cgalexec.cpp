7cd1d84f0df01119a067f5197ded5acbf823ef83
SpyrosPapalexis <sdi1900148@di.uoa.gr>
2024-10-27 19:07:04 +0200

    comments and some fixes on steiner points

--- cgalexec.cpp
+++ cgalexec.cpp
@@ -24,14 +24,14 @@ typedef CDT::Face_handle Face_handle;
 typedef CDT::Edge_iterator Edge_iterator;
 typedef K::Line_2 Line;
 
-#define BUFFER 1024
-
 using namespace std;
 using namespace boost::json;
 
 
 int find_point_index(const vector<Point>& points, const Point& target){
+    //search for the target point in the vector
     auto it = find(points.begin(), points.end(), target);
+    //check if the target was found
     if (it != points.end()){
         return distance(points.begin(), it);
     }
@@ -41,37 +41,30 @@ int find_point_index(const vector<Point>& points, const Point& target){
 
 
 int make_json(std::string instance_uid, int num_points, vector<Point> points, vector<Segment> segments){
+    //set the content type in a string for a cleaner code :)
     std::string content_type = "CG_SHOP_2025_Solution";
 
+    //use a stringstream to construct the JSON file contents
     std::stringstream ss;
     ss << "{\n";
     ss << "  \"content_type\": \"" << content_type << "\",\n";
     ss << "  \"instance_uid\": \"" << instance_uid << "\",\n";
 
-
+    //arrays to store coordinates from Steiner points
     boost::json::array steiner_points_x;
-    for (auto it = points.begin() + num_points; it != points.end(); ++it) {
-
-        if (it != points.end()) {
-            steiner_points_x.push_back(value((*it)[0]));
-        }
+    boost::json::array steiner_points_y;
+    for (auto it = points.begin() + num_points; it != points.end(); ++it){ //steiner points are offset by numpoints in points vector
+        steiner_points_x.push_back(value((*it)[0]));
+        steiner_points_y.push_back(value((*it)[1]));
     }
     
     ss << "  \"steiner_points_x\": [";
-    for (size_t i = 0; i < steiner_points_x.size(); ++i) {
+    for (size_t i = 0; i < steiner_points_x.size(); ++i){
         ss << steiner_points_x[i];
         if (i < steiner_points_x.size() - 1) ss << ", ";
     }
     ss << "],\n";
 
-
-    boost::json::array steiner_points_y;
-    for (auto it = points.begin() + num_points; it != points.end(); ++it){
-
-        if (it != points.end()) {
-            steiner_points_y.push_back(value((*it)[1]));
-        }
-    }
     
     ss << "  \"steiner_points_y\": [";
     for (size_t i = 0; i < steiner_points_y.size(); ++i){
@@ -83,9 +76,11 @@ int make_json(std::string instance_uid, int num_points, vector<Point> points, ve
 
     ss << "  \"edges\": [\n";
     for (size_t i = 0; i < segments.size(); ++i){
+        //find the index of each segment endpoint in the point vector
         int index1 = find_point_index(points, segments[i].source());
         int index2 = find_point_index(points, segments[i].target());
 
+        // check if both points are valid and add them to the stringstream
         if (index1 != -1 && index2 != -1) {
             ss << "    [" << index1 << ", " << index2 << "]";
             if (i < segments.size() - 1) ss << ",";
@@ -95,14 +90,15 @@ int make_json(std::string instance_uid, int num_points, vector<Point> points, ve
     ss << "  ]\n";
     ss << "}\n";
 
+    //try to write output.json
     try{
         std::ofstream output_file("output.json");
         output_file << ss.str();
         output_file.close();
         cout << "output.json created successfully" << endl;
-    } catch (const std::exception &e) {
+    } catch (const std::exception &e){
         cerr << "error at creating JSON file: " << e.what() << endl;
-        return 1;
+        return -1;
     }
 
     return 0;
@@ -111,19 +107,26 @@ int make_json(std::string instance_uid, int num_points, vector<Point> points, ve
 
 
 CDT constrained_delaunay_function(vector<Point> points, const vector<int> region_boundary, vector<vector<int>> additional_constraints, Polygon &polygon){
+    //initialize a Constrained Delaunay Triangulation object
     CDT cdt;
 
+    //insert the edges of the region boundary as constraints in the triangulation
     for (size_t i = 0; i < region_boundary.size(); ++i){
+        //get the indices of two consecutive points
         int a = region_boundary[i];
-        int b = region_boundary[(i + 1) % region_boundary.size()];
+        int b = region_boundary[(i + 1) % region_boundary.size()]; //wraps around
+        //add the point to the polygon and the segment to the constraints
         polygon.push_back(points[a]);
         cdt.insert_constraint(points[a], points[b]);
     }
 
+    //insert all points in the triangulation
     for (const auto& pt : points){
         cdt.insert(pt);
     }
 
+
+    //insert the additional constraints in the triangulation
     for (const auto& constraint : additional_constraints){
         int a = constraint[0];
         int b = constraint[1];
@@ -136,7 +139,10 @@ CDT constrained_delaunay_function(vector<Point> points, const vector<int> region
 
 
 vector<Segment> get_segments(const CDT& cdt){
+    //vector to store segments
     vector<Segment> segments;
+
+    //iterate all the edges in the cdt to push them as segments
     for (Edge_iterator edge = cdt.finite_edges_begin(); edge != cdt.finite_edges_end(); ++edge){
         segments.push_back(cdt.segment(*edge));
     }
@@ -146,10 +152,12 @@ vector<Segment> get_segments(const CDT& cdt){
 
 
 bool is_obtuse_triangle(const Face_handle face, const Polygon polygon){
+    //retrieve the three points of the triangle
     Point p1 = face->vertex(0)->point();
     Point p2 = face->vertex(1)->point();
     Point p3 = face->vertex(2)->point();
 
+    //check for points lies outside the boundary
     if (polygon.bounded_side(p1) != CGAL::ON_BOUNDED_SIDE &&
         polygon.bounded_side(p1) != CGAL::ON_BOUNDARY){
         return false;
@@ -163,19 +171,23 @@ bool is_obtuse_triangle(const Face_handle face, const Polygon polygon){
         return false;
     }
 
+    //find the centroid of the triangle and check if it is within the boundary
     Point centroid = CGAL::centroid(p1, p2, p3);
     if (polygon.bounded_side(centroid) != CGAL::ON_BOUNDED_SIDE){
         return false;
     }
 
+    //calculate the vectors for the triangle's edges
     K::Vector_2 v1 = p2 - p1;
     K::Vector_2 v2 = p3 - p1;
     K::Vector_2 v3 = p3 - p2;
 
+    //calculate the dot products
     double dot1 = v1 * v2;
     double dot2 = -v1 * v3;
     double dot3 = v2 * v3;
 
+    //obtuse angle has negative dot product
     return (dot1 < 0 || dot2 < 0 || dot3 < 0);
 }
 
@@ -185,11 +197,12 @@ int count_obtuse_triangles(const CDT& cdt, Polygon polygon){
     int obtuse_triangle_count = 0;
     int triangle_count = 0;
 
+    //iterate all the faces in the cdt
     for (auto face = cdt.finite_faces_begin(); face != cdt.finite_faces_end(); ++face){
         triangle_count++;
-        if (is_obtuse_triangle(face, polygon)){
-            obtuse_triangle_count++;
-        }
+
+        //check if the current triangle is obtuse
+        if (is_obtuse_triangle(face, polygon)) obtuse_triangle_count++;
     }
 
     cout << "total triangles: " << triangle_count << endl;
@@ -199,7 +212,10 @@ int count_obtuse_triangles(const CDT& cdt, Polygon polygon){
 
 
 Face_handle find_obtuse_triangle(CDT& cdt, Polygon polygon){
+    //iterate all the faces in the cdt
     for (auto face = cdt.finite_faces_begin(); face != cdt.finite_faces_end(); ++face){
+
+        //check if the current triangle is obtuse
         if (is_obtuse_triangle(face, polygon)) return face;
     }
     return nullptr;
@@ -207,112 +223,153 @@ Face_handle find_obtuse_triangle(CDT& cdt, Polygon polygon){
 
 
 
+bool valid_steiner(Point steiner_point, const CDT& cdt){
+    //iterate all finite edges in the cdt
+    for (auto edge = cdt.finite_edges_begin(); edge != cdt.finite_edges_end(); ++edge){
+        //get the source and target points of the edge
+        Point source = cdt.segment(*edge).source();
+        Point target = cdt.segment(*edge).target();
+        if (source == steiner_point || target == steiner_point){
+            return false; //if point already exists in the cdt
+        }
+    }
+    return true; //if point does not exist in the cdt
+}
+
 Point steiner_at_midpoint(CDT& cdt, Polygon polygon){
+    //find an obtuse triangle in the cdt
     Face_handle triangle = find_obtuse_triangle(cdt, polygon);
+
+    //if no obtuse triangle is found, return null point
     if (triangle == nullptr){
         return Point(nan(""), nan(""));
     }
 
+    //retrieve the three points of the triangle
     Point p1 = triangle->vertex(0)->point();
     Point p2 = triangle->vertex(1)->point();
     Point p3 = triangle->vertex(2)->point();
 
+    //calculate the vectors for the triangle's edges
     K::Vector_2 v1 = p2 - p1;
     K::Vector_2 v2 = p3 - p1;
     K::Vector_2 v3 = p3 - p2;
 
+    //calculate the dot products
     double dot1 = v1 * v2;
     double dot2 = -v1 * v3;
     double dot3 = v2 * v3;
 
     Point steiner_point;
-    if (dot1 < 0){
-        steiner_point = CGAL::midpoint(p2, p3);
-    }
-    else if (dot2 < 0){
-        steiner_point = CGAL::midpoint(p1, p3);
-    }
-    else if (dot3 < 0){
-        steiner_point = CGAL::midpoint(p1, p2);
-    }
 
-    cdt.insert(steiner_point);
-    return steiner_point;
+    //determine which angle is obtuse and calculate the midpoint of the corresponding edge
+    if (dot1 < 0) steiner_point = CGAL::midpoint(p2, p3);
+    else if (dot2 < 0) steiner_point = CGAL::midpoint(p1, p3);
+    else if (dot3 < 0) steiner_point = CGAL::midpoint(p1, p2);
+
+    //check if point already exists there
+    if (valid_steiner(steiner_point, cdt)){
+        cdt.insert(steiner_point);
+        return steiner_point;
+    }
+    return Point(nan(""), nan(""));
 }
 
 
 
 Point steiner_at_circumcenter(CDT& cdt, Polygon polygon){
+    //find an obtuse triangle in the cdt
     Face_handle triangle = find_obtuse_triangle(cdt, polygon);
+
+    //if no obtuse triangle is found, return null point
     if (triangle == nullptr){
         return Point(nan(""), nan(""));
     }
 
+    //retrieve the three points of the triangle
     Point p1 = triangle->vertex(0)->point();
     Point p2 = triangle->vertex(1)->point();
     Point p3 = triangle->vertex(2)->point();
 
     Point steiner_point = CGAL::circumcenter(p1, p2, p3);
 
-    cdt.insert(steiner_point);
-    return steiner_point;
+    //check if point already exists there
+    if (valid_steiner(steiner_point, cdt)){
+        cdt.insert(steiner_point);
+        return steiner_point;
+    }
+    return Point(nan(""), nan(""));
 }
 
 
 
 Point steiner_at_projection(CDT& cdt, Polygon polygon){
+    //find an obtuse triangle in the cdt
     Face_handle triangle = find_obtuse_triangle(cdt, polygon);
+
+    //if no obtuse triangle is found, return null point
     if (triangle == nullptr){
         return Point(nan(""), nan(""));
     }
 
-    Point a = triangle->vertex(0)->point();
-    Point b = triangle->vertex(1)->point();
-    Point c = triangle->vertex(2)->point();
+    //retrieve the three points of the triangle
+    Point p1 = triangle->vertex(0)->point();
+    Point p2 = triangle->vertex(1)->point();
+    Point p3 = triangle->vertex(2)->point();
 
-    K::Vector_2 v1 = b - a;
-    K::Vector_2 v2 = c - a;
-    K::Vector_2 v3 = c - b;
+    //calculate the vectors for the triangle's edges
+    K::Vector_2 v1 = p2 - p1;
+    K::Vector_2 v2 = p3 - p1;
+    K::Vector_2 v3 = p3 - p2;
 
+    //calculate the dot products
     double dot1 = v1 * v2;
     double dot2 = -v1 * v3;
     double dot3 = v2 * v3;
 
+    //determine which angle is obtuse and calculate the projection on the corresponding edge
     Point steiner_point;
     if (dot1 < 0){
-        Line line(b, c);
-        steiner_point = line.projection(a);
+        Line line(p2, p3);
+        steiner_point = line.projection(p1);
     }
     else if (dot2 < 0){
-        Line line(a, c);
-        steiner_point = line.projection(b);
+        Line line(p1, p3);
+        steiner_point = line.projection(p2);
     }
     else if (dot3 < 0){
-        Line line(a, b);
-        steiner_point = line.projection(c);
+        Line line(p1, p2);
+        steiner_point = line.projection(p3);
     }
 
-    cdt.insert(steiner_point);
-    return steiner_point;
+    //check if point already exists there
+    if (valid_steiner(steiner_point, cdt)){
+        cdt.insert(steiner_point);
+        return steiner_point;
+    }
+    return Point(nan(""), nan(""));
 }
 
 
 
 int main(void){
-
-    char filename[BUFFER] = "";
-
+    //file name insert to open
+    std::string filename;
     cout << "Enter file name:" << endl;
     cin >> filename;
 
-std::ifstream file(filename);
-    if (!file.is_open()) {
+    //open the file and check if it opened successfully
+    std::ifstream file(filename);
+    if (!file.is_open()){
         cerr << "Error opening file." << endl;
         return 1;
     }
+
+    //read the JSON file as a string
     std::string jsonStr((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     file.close();
 
+    //parse the string into JSON data
     value jsonData;
     try {
         jsonData = parse(jsonStr);
@@ -321,8 +378,10 @@ std::ifstream file(filename);
         return 2;
     }
 
+    //access the JSON data as object
     object& obj = jsonData.as_object();
 
+    //read the variables
     std::string instance_uid = obj["instance_uid"].as_string().c_str();
     int num_points = obj["num_points"].as_int64();
 
@@ -336,6 +395,7 @@ std::ifstream file(filename);
         points_y.push_back(item.as_int64());
     }
 
+    //combine x and y coordinates to create Points
     vector<Point> points;
     for (int i = 0; i < num_points; ++i) {
         points.push_back(Point(points_x[i], points_y[i]));
@@ -357,13 +417,16 @@ std::ifstream file(filename);
         additional_constraints.push_back(constraint);
     }
 
+    //create Constrained Delaunay Triangulation (cdt) and Polygon for region boundary
     Polygon polygon;
     CDT cdt = constrained_delaunay_function(points, region_boundary, additional_constraints, polygon);
 
+    //count obtuse triangles and draw the cdt
     int obtuse_triangle_count = count_obtuse_triangles(cdt, polygon);
     cout << "Obtuse triangle count is: " << obtuse_triangle_count << endl;
     CGAL::draw(cdt);
 
+    //select steiner point placement method
     int method;
     cout << "Enter method number (1,2,3)" << endl;
     cin >> method;
@@ -372,26 +435,28 @@ std::ifstream file(filename);
     cout << "Enter number of desired steiner points:" << endl;
     cin >> steiner_max;
 
+    //insert steiner points based on the selected method
     for (int i = 0; i < steiner_max; i++){
         Point steiner_point;
         if (method == 1) steiner_point = steiner_at_midpoint(cdt, polygon);
         else if (method == 2) steiner_point = steiner_at_circumcenter(cdt, polygon);
         else if (method == 3) steiner_point = steiner_at_projection(cdt, polygon);
-        else if (method == 3); //steiner_point = steiner_at_3(cdt, polygon);
         else{
             cout << "Wrong method imput." << endl;
             return 3;
         }
-        
+        //add Steiner point only if it is not null
         if (steiner_point[0] != nan("") && steiner_point[1] != nan("")) points.push_back(steiner_point);
-        else break;
+        else break; //break if no valid steiner points are left
     }
+
+    //recount obtuse triangles after inserting steiner points and redraw
     obtuse_triangle_count = count_obtuse_triangles(cdt, polygon);
     cout << "Obtuse triangle count is: " << obtuse_triangle_count << endl;
     CGAL::draw(cdt);
 
+    //create segments from cdt for the output file
     vector<Segment> segments = get_segments(cdt);
-
     make_json(instance_uid, num_points, points, segments);
 
     return 0;
